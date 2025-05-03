5afb20d8672ad23239bda3e7e81d01aca6ac6f69
sbk2k1 <bhattacharyasaptarshi2001@gmail.com>
2025-05-03 20:13:43 +0530

    Test setup initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello World{}", "!");
+    println!("Hello World{}", "!");
+    println!("Hello World{}", "!");
+    println!("Hello World{}", "!");
+    println!("Hello World{}", "!");
+
 
     loop {}
 }
-
