5c2e4757fbe3ad12d80b1c0408613c27336f8ede
sbk2k1 <bhattacharyasaptarshi2001@gmail.com>
2025-05-03 14:55:22 +0530

    VGA Writing setup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
@@ -9,18 +10,10 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
+
